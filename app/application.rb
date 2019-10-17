e7f6fd5a975c7a4d06d94738ca84ecee99954b89
Martin Moy <martinmoy1991@gmail.com>
2019-10-17 02:05:33 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,9 +14,9 @@ class Application
     elsif req.match(/cart/)
       if @@cart.empty?
           resp.write "Your cart is empty"
-      
 
-      
+
+
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
